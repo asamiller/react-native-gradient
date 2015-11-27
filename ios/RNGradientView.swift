75d06c28abe266048e694f09107082e074b29639
Jason Brown <browniefed@gmail.com>
2015-11-26 19:14:41 -0800

    cleanup commented out code

--- ios/RNGradientView.swift
+++ ios/RNGradientView.swift
@@ -25,24 +25,4 @@ class RNGradientView : GradientView {
   func setLocations(locations: NSArray) {
     self.locations = locations.map({ return $0 as! CGFloat});
   }
-  
-//  override func layoutSubviews() {
-//    let gradientView = GradientView(frame: self.bounds)
-//        // Set the gradient colors
-//    gradientView.colors = [UIColor.blueColor(), UIColor.yellowColor()]
-//    
-//    // Optionally set some locations
-//    gradientView.locations = [0.8, 1.0]
-//    
-//    // Optionally change the direction. The default is vertical.
-//    //    gradientView.direction = .Horizontal
-//    
-//    // Add some borders too if you want
-//    //    gradientView.topBorderColor = UIColor.redColor()
-//    //    gradientView.bottomBorderColor = UIColor.blueColor()
-//
-//    self.addSubview(gradientView);
-//  }
-
-
 }
